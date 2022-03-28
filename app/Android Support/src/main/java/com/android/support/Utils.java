b897cba117cac63d9161aa9cfb67666319fa6b00
sahil78698 <sahilhossain999coc@gmail.com>
2022-03-28 14:10:29 +0530

    Updated

--- app/Android Support/src/main/java/com/android/support/Utils.java
+++ app/Android Support/src/main/java/com/android/support/Utils.java
@@ -11,18 +11,17 @@ public class Utils {
     public static void Support() {
         bundle = Support.Main_bundle;
         if (Support.Main_bundle == null) {
+            Loader.load();
             Log.e("Utils Error", "Not Loaded");
         } else {
-            if (bundle != null) {
-                if (bundle.getString("appKey").equals(Push.Key())) {
-                    if (bundle.getString("m_i").equals(bundle.getString("a_i")) && bundle.getString("r_i").equals("l_i")) {
-                        Android.Init(Support.Main_context, bundle.getString("current_game"), bundle.getString("profile_name"), bundle.getString("profile_url"), Support.per, Support.progressBar);
-                    } else {
-                        Loader.load();
-                    }
+            if (bundle.getString("appKey").equals(Push.Key())) {
+                if (bundle.getString("m_i").equals(bundle.getString("a_i")) && bundle.getString("r_i").equals("l_i")) {
+                    Android.Init(Support.Main_context, bundle.getString("current_game"), bundle.getString("profile_name"), bundle.getString("profile_url"), Support.per, Support.progressBar);
                 } else {
                     Loader.load();
                 }
+            } else {
+                Loader.load();
             }
         }
     }
